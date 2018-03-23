1aa3cc3ea41ccc189314c393e9df552e28afa119
Arnav Ghosh <ag983@cornell.edu>
2018-03-23 15:31:16 -0400

    SVM Para Search as well - for feature selection

--- baselines/baselines.py
+++ baselines/baselines.py
@@ -3,8 +3,12 @@
 #Arnav Ghosh
 
 import eval
+import pandas as pd
 from sklearn import svm
 from sklearn.naive_bayes import MultinomialNB
+
+SVM_C_INIT = [1, 10, 100, 1000, 10000]
+SVM_MAX_ITER = [1, 50, 100, 200, 500, 1000]
 
 def createBayesClassifier(dataMatrix, yTr, dupRatio):
 	clf = MultinomialNB(class_prior = [1 - dupRatio, dupRatio])
@@ -18,6 +22,24 @@ def createSVMClassifier(xTrain, yTrain, C, maxIter):
 
 def scoreClassifier(classifier, xTe, yTe):
 	preds = classifier.predict(xTe)
-	precision, recall, f1 = eval.getScores(preds, yTe)
+	precision, recall, f1 = eval.getScores(yTe, preds)
 
 	return precision, recall, f1
+
+def svmParaSearch(xTrain, yTrain, xTe, yTe):
+	results = {"C" : [], "iter": [], "precision": [], "recall" : [], "f1": []}
+
+	for c in SVM_C_INIT:
+		for i in SVM_MAX_ITER:
+			clf = createSVMClassifier(xTrain, yTrain, c, i)
+			precision, recall, f1 = scoreClassifier(clf, xTe, yTe)
+			results["C"] = results["C"] + [c]
+			results["iter"] = results["iter"] + [i]
+			results["precision"] = results["precision"] + [precision]
+			results["recall"] = results["recall"] + [recall]
+			results["f1"] = results["f1"] + [f1]
+
+	return pd.DataFrame(data = results)
+
+
+
